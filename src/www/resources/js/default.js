f9f2e1bed631c00a2b20b8348f9a2881c480f7f3
Trygve Lie <github@trygve-lie.com>
2011-05-12 07:57:26 +0200

    Getting closer to a working design

---
+++ src/www/resources/js/default.js
@@ -0,0 +1,6 @@
+document.createElement('header');
+document.createElement('section');
+document.createElement('article');
+document.createElement('nav');
+document.createElement('time');
+document.createElement('footer');
